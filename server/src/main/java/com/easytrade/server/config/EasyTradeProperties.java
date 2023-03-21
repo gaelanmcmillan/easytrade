b39b524b5408ff7c94a68288504ca37b254747a0
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-03-21 11:29:32 -0400

    Collapse ApplicationConfig into SecurityConfig and EasyTradeProperties

--- server/src/main/java/com/easytrade/server/config/EasyTradeProperties.java
+++ server/src/main/java/com/easytrade/server/config/EasyTradeProperties.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Configuration
 @ConfigurationProperties(prefix="easytrade")
 public class EasyTradeProperties {
     private String exchange;
