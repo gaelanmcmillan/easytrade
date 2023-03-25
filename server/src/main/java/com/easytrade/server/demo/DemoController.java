eb88ae441c6a72d82edca3f2dff64ca4f507e2a7
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-03-25 17:42:06 -0400

    Make demo controller cors compatible

--- server/src/main/java/com/easytrade/server/demo/DemoController.java
+++ server/src/main/java/com/easytrade/server/demo/DemoController.java
@@ -3,6 +3,7 @@ package com.easytrade.server.demo;
 import com.easytrade.server.config.EasyTradeProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.Calendar;
 import java.util.stream.Collectors;
 
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/demo")
 @RequiredArgsConstructor
