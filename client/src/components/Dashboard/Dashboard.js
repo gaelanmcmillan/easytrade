670964c821dc5c47d4038d4cace0840746d140a7
OriginalOrangeXD <deangelis.robert13@gmail.com>
2023-03-25 17:41:14 -0400

    css and dashboard

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Dashboard.css';
+const hostname = "http://localhost:8080"
+const apiPrefix = hostname + "/api/v1"
+function request (data) {
+  console.log(data.token+"TOKEN");
+  return {
+    method: "GET",
+    headers: { "Authorization": "Bearer "+data.token, "Accept":"application/json" }
+  }
+}
+/**
+ * Send a request to the backend to log the user in.
+ *
+ * @param {*} userData { email, password }
+ * @param {*} responseHandler A callback function to use with the response. Cache username, token, expiry.
+ */
+const demo = async (demoData, responseHandler) => {
+  const endpoint = apiPrefix + "/auth/demo";
+  fetch(endpoint, request(demoData))
+    .then(res => res.json())
+    .then(data => responseHandler(data));
+}
+
+class Dashboard extends React.Component {
+    constructor ( getToken ) {
+        super(getToken);
+        this.getToken = getToken.getToken;
+        const demoData = {
+            token: this.getToken(),
+        };
+        demo(demoData, data => console.log(data));
+    }
+    render () {
+        return (
+            <div className='dashboard'>
+                <p>####Dashbard####</p>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
